af969497017c26631a2206871913c2ef08f090ac
Burhan <62214284+Burhan-Q@users.noreply.github.com>
2023-10-28 18:24:36 -0400

    add cfg loading

--- src/UltralyticsBot/__init__.py
+++ src/UltralyticsBot/__init__.py
@@ -8,7 +8,14 @@ Requires: discord.py, pyyaml, numpy, requests, opencv-python
 
 from pathlib import Path
 
+import yaml
+
 ROOT = Path(__file__).parent
 PROJ_ROOT = ROOT.parent.parent
 
-__all__ = 'ROOT', 'PROJ_ROOT'
+SECRETS = yaml.safe_load((PROJ_ROOT / 'SECRETS/codes.yaml').read_text('utf-8'))
+CMDS = yaml.safe_load((PROJ_ROOT / 'cfg/commands.yaml').read_text('utf-8'))
+REQ_CFG = yaml.safe_load((PROJ_ROOT / 'cfg/req.yaml').read_text('utf-8'))
+ASSETS = PROJ_ROOT / 'assets'
+
+__all__ = 'ROOT', 'PROJ_ROOT', 'SECRETS', 'CMDS', 'REQ_CFG', 'ASSETS'
