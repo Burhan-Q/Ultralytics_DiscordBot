9f909c3c69fde3393fee9e5623c856b59eb84048
Burhan <62214284+Burhan-Q@users.noreply.github.com>
2023-10-29 12:47:46 -0400

    centralize constants update imports

--- src/UltralyticsBot/__init__.py
+++ src/UltralyticsBot/__init__.py
@@ -12,10 +12,21 @@ import yaml
 
 ROOT = Path(__file__).parent
 PROJ_ROOT = ROOT.parent.parent
+GH = "https://github.com/Burhan-Q/Ultralytics_DiscordBot"
 
 SECRETS = yaml.safe_load((PROJ_ROOT / 'SECRETS/codes.yaml').read_text('utf-8'))
+BOT_TOKEN = SECRETS['apikey']
+BOT_ID = SECRETS['botID']
+HUB_KEY = SECRETS['inferkey']
+
 CMDS = yaml.safe_load((PROJ_ROOT / 'cfg/commands.yaml').read_text('utf-8'))
+
 REQ_CFG = yaml.safe_load((PROJ_ROOT / 'cfg/req.yaml').read_text('utf-8'))
+DEFAULT_INFER = REQ_CFG['default']
+REQ_ENDPOINT = REQ_CFG['endpoint']
+REQ_LIM = REQ_CFG['limits']
+RESPONSE_KEYS = tuple(REQ_CFG['response'])
+
 ASSETS = PROJ_ROOT / 'assets'
 
-__all__ = 'ROOT', 'PROJ_ROOT', 'SECRETS', 'CMDS', 'REQ_CFG', 'ASSETS'
+__all__ = 'ROOT', 'PROJ_ROOT', 'SECRETS', 'CMDS', 'REQ_CFG', 'ASSETS', 'BOT_TOKEN', 'BOT_ID', 'HUB_KEY', 'DEFAULT_INFER', 'REQ_ENDPOINT', 'REQ_LIM', 'RESPONSE_KEYS', 'GH'
