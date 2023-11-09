7a14c1e46252ce78f711292017caf7674b42fbb9
Burhan <62214284+Burhan-Q@users.noreply.github.com>
2023-11-09 14:24:58 -0500

    task loop new docs daily into client, fix cmd_sync

--- src/UltralyticsBot/cmds/client.py
+++ src/UltralyticsBot/cmds/client.py
@@ -5,24 +5,46 @@ Date: 2023-10-29
 
 Requires: discord.py
 """
+import datetime
 
 import discord
 from discord import app_commands
+from discord.ext import tasks
 
-from UltralyticsBot import CMDS
+from UltralyticsBot import CMDS, DEV_CH
 from UltralyticsBot.utils.logging import Loggr
+from UltralyticsBot.utils.docs_data import docs_choices, load_docs_cache
+
+RUN_AT = datetime.time(hour=0, minute=0, second=0, tzinfo=datetime.timezone.utc) # time to refresh repo and docs
 
 class MyClient(discord.Client):
     """Class for Discord application/bot with slash-commands, requires message content intents"""
     def __init__(self, *, intents:discord.Intents):
         super().__init__(intents=intents)
         self.tree = app_commands.CommandTree(self)
+        self.docs_choices, self.docs_embeds = load_docs_cache()
         self.cmd_pop()
     
     async def setup(self):
         """Sync commands, could take upto an hour to show up when bot is in lots of servers"""
         Loggr.info(f"Intitating client sync.")
         await self.tree.sync()
+    
+    async def setup_hook(self) -> None:
+        # return await super().setup_hook()
+        self.docs_update.start()
+    
+    @tasks.loop(time=RUN_AT)
+    async def docs_update(self):
+        """Task loop to update Documentation commands."""
+        notice_ch = self.get_channel(DEV_CH)
+        Loggr.info(f"Running scheduled docs command.")
+        self.docs_choices, self.docs_embeds = docs_choices()
+        await notice_ch.send(content=f"Docs update task completed.")
+
+    @docs_update.before_loop
+    async def before_my_task(self):
+        await self.wait_until_ready()
     
     def cmd_pop(self, cmds:dict=CMDS):
         """Populate client with commands from YAML file."""
